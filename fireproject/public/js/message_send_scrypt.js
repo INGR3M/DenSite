66951005eb80504b0244c82de5edcde760d467e1
INGR3M <heavenitman@gmail.com>
2025-03-07 12:29:45 +0400

    валидация почты

--- fireproject/public/js/message_send_scrypt.js
+++ fireproject/public/js/message_send_scrypt.js
@@ -1,3 +1,9 @@
+function validateEmail(email) {
+    var re = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
+    return re.test(String(email).toLowerCase());
+}
+
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -7,22 +13,26 @@ document.getElementById('contactForm').addEventListener('submit', function(event
       email: document.getElementById('email').value,
       message: document.getElementById('message').value
   };
-
-  fetch('/send-message', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData)
-  })
-  .then(response => response.json())
-  .then(result => {
-      alert(result.message || 'Заявка отправлена! Теперь вы можете скачать каталог.');
-      
-  })
-  .then(document.getElementById('catalog-btn').setAttribute('onclick', 'alert("Кнопка нажата")'))
-  .catch(error => {
-      console.error('Ошибка:', error);
-      alert('Произошла ошибка, попробуйте снова.');
-  });
-
-  document.getElementById('contactForm').reset();
+  if (validateEmail(document.getElementById('email').value)){
+    fetch('/send-message', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(result => {
+        alert(result.message || 'Заявка отправлена! Теперь вы можете скачать каталог.');
+        
+    })
+    .then(document.getElementById('catalog-btn').setAttribute('onclick', 'alert("Кнопка нажата")'))
+    .catch(error => {
+        console.error('Ошибка:', error);
+        alert('Произошла ошибка, попробуйте снова.');
+    });
+  
+    document.getElementById('contactForm').reset();
+  }
+  else{
+    alert("Почта введена некорректно!")
+  }
 });
